868c6b4ecfe6b205855ff6eb4f4719e10b6a2c17
Alexandra Østermark <alex.cramt@gmail.com>
2024-05-03 18:45:40 +0200

    neorg works

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,53 @@
+return {
+  "nvim-neorg/neorg",
+  version = "^8",
+  dependencies = {
+    -- versions pulled from here https://github.com/nvim-neorg/neorg/blob/03fb74927f358320f66cdd1337265c8e7f049fa3/build.lua#L16
+    {
+      "nvim-neotest/nvim-nio",
+      version = "v1.7.0",
+    },
+    {
+      "nvim-neorg/lua-utils.nvim",
+      version = "v1.0.2",
+    },
+    {
+      "MunifTanjim/nui.nvim",
+      version = "v0.3.0",
+    },
+    {
+      "nvim-lua/plenary.nvim",
+      version = "v0.1.4",
+    },
+    {
+      "pysan3/pathlib.nvim",
+      version = "v2.2.2",
+    },
+    {
+      "vhyrro/luarocks.nvim",
+      priority = 1000, -- We'd like this plugin to load first out of the rest
+      config = false,
+    },
+  },
+  event = "VeryLazy",
+  opts = {
+    load = {
+      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.keybinds"] = {}, -- Adds default keybindings
+      ["core.completion"] = {
+        config = {
+          engine = "nvim-cmp",
+        },
+      }, -- Enables support for completion plugins
+      ["core.journal"] = {}, -- Enables support for the journal module
+      ["core.dirman"] = { -- Manages Neorg workspaces
+        config = {
+          workspaces = {
+            notes = "~/projects/notes",
+          },
+        },
+      },
+    },
+  },
+}
